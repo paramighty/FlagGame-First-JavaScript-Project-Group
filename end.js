4be1df059c151807ac2842aa77370ae9da589b9f
Staffan Ösp <staffan.osp@gmail.com>
2022-11-24 09:24:14 +0100

    highscore styling WIP

--- end.js
+++ end.js
@@ -4,7 +4,7 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const max_high_scores = 7;
 
@@ -20,18 +20,25 @@ saveHighScore = (e) => {
   console.log("clicked the button");
   e.preventDefault();
 
+  highScores = highScores.map((v) => {
+    v.mostRecent = false;
+    return v;
+  });
+
   const score = {
+    mostRecent: true,
     score: mostRecentScore,
     name: username.value,
   };
+
   highScores.push(score);
 
   highScores.sort((a, b) => b.score - a.score);
 
-  highScores.splice(5);
+  highScores.splice(10);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  //window.location.assign("/");
+
   //Takes you to leaderboard
   localStorage.setItem("highScoresList", score);
   return window.location.assign("highscores.html");
--- highscores.js
+++ highscores.js
@@ -1,8 +1,25 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const playAgainBtnEl = document.getElementById("playAgainBtn");
+const highScoreListContainerEl = document.getElementById(
+  "highscore-list-container"
+);
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
-  .map((score) => {
-    return `<li class="high-score">${score.name}-${score.score}</li>`;
-  })
-  .join("");
+for (let [i, score] of highScores.entries()) {
+  let itemClass = ["highscore-list-container__item"];
+
+  if (score.mostRecent) {
+    itemClass.push("highscore-list-container__item--emphasis");
+    itemClass.push("animate__animated");
+    itemClass.push("animate__tada");
+  }
+
+  highScoreListContainerEl.innerHTML += `
+  <div class="${itemClass.join(" ")}">
+    <div>${i + 1}</div>
+    <div>${score.name}</div>
+    <div>${score.score}</div>
+  </div>
+  `;
+}
+
+playAgainBtnEl.addEventListener("click", () => window.location.assign("/"));
