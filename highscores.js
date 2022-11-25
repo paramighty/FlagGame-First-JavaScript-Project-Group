750cb92c199ec2b34033cf0983cd099b3c0213cc
Staffan Ösp <staffan.osp@gmail.com>
2022-11-25 13:45:12 +0100

    add icons to highscore list

--- highscores.js
+++ highscores.js
@@ -4,6 +4,22 @@ const highScoreListContainerEl = document.getElementById(
 );
 let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+function getRankIcon(rank) {
+  const rankIcons = {
+    1: { icon: "🥇", scale: 2 },
+    2: { icon: "🥈", scale: 1.5 },
+    3: { icon: "🥉", scale: 1 },
+  };
+
+  const rankIcon = rankIcons[rank];
+
+  if (rankIcon) {
+    return `<span class="emoji" style="scale:${rankIcon.scale}">${rankIcon.icon}</span>`;
+  }
+
+  return rank;
+}
+
 for (let [i, score] of highScores.entries()) {
   let itemClass = ["highscore-list-container__item"];
 
@@ -15,7 +31,7 @@ for (let [i, score] of highScores.entries()) {
 
   highScoreListContainerEl.innerHTML += `
   <div class="${itemClass.join(" ")}">
-    <div>${i + 1}</div>
+    <div>${getRankIcon(i + 1)}</div>
     <div>${score.name}</div>
     <div>${score.score}</div>
   </div>
